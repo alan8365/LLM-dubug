5f206c26b2e3b273966a0a190ca68e51f9de2f43
Lucy mac <alan8365@gmail.com>
2023-11-27 09:55:47 +0800

    init

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+import os
+import re
+import shutil
+from glob import glob
+from pathlib import Path
+
+
 def extract_def_function(code: str):
     # function to extract def function block from py file
     def_block = []
@@ -7,12 +14,49 @@ def extract_def_function(code: str):
         if line.startswith("def"):
             def_block.append(line)
 
-
     return def_block
 
 
-if __name__ == '__main__':
-    with open("main.py", "r") as f:
-        test_code = f.read()
+def copy_to_custom_folder(generated_code_path: Path, index: int) -> None:
+    """
+    Copy generated code to a custom folder.
 
-    extract_def_function(test_code)
+    Args:
+        generated_code_path: Path to the generated code directory.
+        index: Index of the generated code.
+
+    Returns:
+        None
+    """
+    # Set the destination path
+    custom_folder_path = Path('datasets/QuixBugs/custom_python_programs')
+
+    # Clean the file in dst folder if it exists
+    if custom_folder_path.exists():
+        shutil.rmtree(custom_folder_path)
+
+    # Create the dst folder
+    custom_folder_path.mkdir()
+
+    # Get all the folders in the generated code path
+    all_folders = os.listdir(generated_code_path)
+
+    # Exclude folders starting with '.'
+    all_folders = [folder for folder in all_folders if not folder.startswith('.')]
+
+    # Sort the folders
+    all_folders = sorted(all_folders)
+
+    # Copy the generated code to custom folder
+    for folder in all_folders:
+        source_file = generated_code_path / folder / f'{folder}_{index}.py'
+        destination_file = custom_folder_path / f'{folder}.py'
+        shutil.copyfile(source_file, destination_file)
+
+
+def get_all_function():
+    function_list = glob('datasets/QuixBugs/python_programs/*.py')
+    function_list = [Path(x).stem for x in function_list if not re.match(r'.*test.py', x) and 'node' not in x]
+    function_list = sorted(function_list)
+
+    return function_list
